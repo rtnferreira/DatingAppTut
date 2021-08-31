bbb8cfeca3250fa5164d0b92b27110b9623d4e63
rtnf <rtnferreira@outlook.com>
2021-08-31 23:30:33 +0100

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -34,10 +34,6 @@ export class NavComponent implements OnInit
               console.log(response);
               /* this.loggedIn = true; */
               this.router.navigateByUrl('/members');
-    }, error =>
-    {
-      console.log(error);
-      this.toastr.error(error.error);
     });
   }
 
